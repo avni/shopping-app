f94f4db183fb219e0abc38398c318b2da59fc558
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-09-12 08:58:45 -0400

    Fixed error
    length method does NOT take parentheses in JS.

--- script.js
+++ script.js
@@ -4,28 +4,31 @@
 var list = [];
 
 /* buildList
- * Adds items to an array
+ * Adds items to the end of an array, one at a time.
  * @param: none;
  * @return: none;
  */
-
 function buildList(){
-	alert("Write Items. Type s to stop.");
-	let nextItem = "";
-	while (nextItem != "s") {
-		nextItem = prompt("Next Item?");
-    list.push(nextItem);
-	}
+   alert("Write Items. Type s to stop.");
+   let nextItem = "";
+   while (nextItem != "s") {
+	nextItem = prompt("Next Item?");
+    	list.push(nextItem);
+   }
+   alert("List items entered! Go shop.");
 }	
 
+/* shopList
+ * Removes items from array, 1 by 1, starting from end. 
+ * @param: none;
+ * @return: none;
+ */
 function shopList() {
-  alert("list");
   let item = list.pop();
-  while (list.length() >= 0){
+  alert("Here's your list: "+list.toString());
+  while (list.length > 0){
     item = list.pop();
     alert (item);
-	}  
-	alert("Go check out!");
+  }  
+  alert("Done. Go check out!");
 }
-
- 
